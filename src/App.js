ce29d56628ba8ea95d07e5e5f331ef03fcdc51eb
alesantamaria11 <alesantamariadh@gmail.com>
2022-09-02 17:59:03 -0300

    proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React, { useState } from 'react';
-import Cabecera from './components/Cabecera';
-import Listado from './components/Listado';
-// El componente App es el padre de:
-// - Cabecera
-// - Listado
-// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
-// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
-// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import React, { useState } from 'react'
+import Cabecera from './components/Cabecera'
+import Listado from './components/Listado'
 
 function App() {
 
@@ -18,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <Cabecera/>
+      <Cabecera carrito={carrito}/>
       <Listado carrito={()=> actualizarCarrito()}/>
     </div>
   );
